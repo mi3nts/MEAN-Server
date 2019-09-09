c4056041f3559c5a4e47c3349c33686d4916bc78
xiaohe <xiaohe.yu86@gmail.com>
2019-09-09 18:14:06 -0500

    first

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const mongodb_conn_module = require('./mongodbConnModule');
+var db = mongodb_conn_module.connect();
+
+var Post = require("../models/post");
+
+app.get('/latest', (req, res) => {
+  Post.find({"parameter":"pm25","country":"US","coordinates.latitude":{$exists:true}},"country city date.utc coordinates value parameter", function (error, po) {
+	  if (error) { console.error(error); }
+	  res.send({
+			post: po
+		})
+	}).sort({_id:-1})
+})
+
+app.listen(process.env.PORT || 8081)
